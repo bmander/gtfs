e943f6c3e5abc7a8aae92ffde0b6c54bec5d3c28
Kurt Raschke <kurt@kurtraschke.com>
2011-05-15 23:29:21 -0400

    Code cleanup for PEP 8 compliance.  This includes changing from two spaces for indentation to four.

--- gtfs/feed.py
+++ gtfs/feed.py
@@ -3,34 +3,36 @@ import errno
 from zipfile import ZipFile
 from csv import DictReader
 
+
 class Feed(object):
-  """A Feed is a collection of CSV files with headers, either zipped into an archive
-     or loose in a folder"""
+    """A Feed is a collection of CSV files with headers, either zipped into
+    an archive or loose in a folder"""
 
-  def __init__(self, filename):
-    self.filename = filename 
-    self.zf = None
+    def __init__(self, filename):
+        self.filename = filename
+        self.zf = None
 
-    if not os.path.isdir(filename):
-      self.zf = ZipFile(filename)
+        if not os.path.isdir(filename):
+            self.zf = ZipFile(filename)
 
-  def get_reader(self, filename):
-    if self.zf:
-      try:
-        f = self.zf.open(filename)
-      except KeyError:
-        raise FileNotFoundError("%s not found" % filename)
-    else:
-      try:
-        f = open(os.path.join(self.filename, filename))
-      except IOError, e:
-        if e.errno==errno.ENOENT:
-          raise FileNotFoundError("%s not found" % filename)
+    def get_reader(self, filename):
+        if self.zf:
+            try:
+                f = self.zf.open(filename)
+            except KeyError:
+                raise FileNotFoundError("%s not found" % filename)
         else:
-          raise
-        
-    dr = DictReader(f)
-    return dr
+            try:
+                f = open(os.path.join(self.filename, filename))
+            except IOError, e:
+                if e.errno == errno.ENOENT:
+                    raise FileNotFoundError("%s not found" % filename)
+                else:
+                    raise
+
+        dr = DictReader(f)
+        return dr
+
 
 class FileNotFoundError(Exception):
-  pass
+    pass
--- gtfs/schedule.py
+++ gtfs/schedule.py
@@ -1,48 +1,49 @@
 import sqlalchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from gtfs.entity import *
 
-from entity import *
 
 class Schedule:
-  def __init__(self, db_filename, echo=False):
-    self.db_filename = db_filename
+    def __init__(self, db_filename, echo=False):
+        self.db_filename = db_filename
 
-    self.engine = sqlalchemy.create_engine('sqlite:///%s' % self.db_filename, echo=echo)
+        self.engine = sqlalchemy.create_engine(
+            'sqlite:///%s' % self.db_filename, echo=echo)
 
-    Base.metadata.bind = self.engine
+        Base.metadata.bind = self.engine
 
-    Session = scoped_session(sessionmaker())
-    Base.query = Session.query_property()
-    self.session = Session()
+        Session = scoped_session(sessionmaker())
+        Base.query = Session.query_property()
+        self.session = Session()
 
-  @property
-  def routes(self):
-    return self.session.query(Route).all()
+    @property
+    def routes(self):
+        return self.session.query(Route).all()
 
-  @property
-  def agencies(self):
-    return self.session.query(Agency).all()
+    @property
+    def agencies(self):
+        return self.session.query(Agency).all()
 
-  @property
-  def service_periods(self):
-    return self.session.query(ServicePeriod).all()
+    @property
+    def service_periods(self):
+        return self.session.query(ServicePeriod).all()
 
-  @property
-  def stops(self):
-    return self.session.query(Stop).all()
+    @property
+    def stops(self):
+        return self.session.query(Stop).all()
 
-  @property
-  def trips(self):
-    return self.session.query(Trip).all()
+    @property
+    def trips(self):
+        return self.session.query(Trip).all()
 
-  def service_for_date(self, service_date):
-    active_periods = []
-    for period in self.service_periods:
-      if period.active_on_date(service_date):
-        active_periods.append(period)
-    return active_periods
+    def service_for_date(self, service_date):
+        active_periods = []
+        for period in self.service_periods:
+            if period.active_on_date(service_date):
+                active_periods.append(period)
+        return active_periods
 
-  def create_tables( self ):
-    Base.metadata.create_all()
-    self.session.commit()
+    def create_tables(self):
+        Base.metadata.create_all()
+        self.session.commit()
